7eece7ce515f97f23bc2148cb8e33cdb67d12585
yonatan-nugraha <yonatan.toemali@gojek.com>
2021-09-11 23:23:43 +0700

    init project

---
+++ src/main/java/com/example/pichu/PichuApplication.java
@@ -0,0 +1,13 @@
+package com.example.pichu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PichuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PichuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pichu/PichuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pichu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PichuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
